745eea4f2cc5973fd7225aa626bbc46a9662c6f6
Kuldeep-Singh-20 <kuldeep20jan01@gmail.com>
2025-04-22 23:07:11 +0530

    first commit

--- src/main/java/com/bankingApp/dao/ErrorResponse.java
+++ src/main/java/com/bankingApp/dao/ErrorResponse.java
@@ -3,7 +3,6 @@ package com.bankingApp.dao;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
@@ -15,13 +14,13 @@ public class ErrorResponse {
     private LocalDateTime timestamp;
     private String message;
     private String details;
-    private String erroCode;
+    private String errorCode;
 
-    public ErrorResponse(LocalDateTime timestamp, String message, String details,String erroCode) {
+    public ErrorResponse(LocalDateTime timestamp, String message, String details,String errorCode) {
         this.timestamp = timestamp;
         this.message = message;
         this.details = details;
-        this.erroCode =erroCode;
+        this.errorCode =errorCode;
     }
 
 }
